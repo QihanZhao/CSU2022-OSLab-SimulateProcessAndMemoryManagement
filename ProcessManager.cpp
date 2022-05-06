e81d9e2293e906b8467ab9be94ac0984f23805b5
HanX-gthb <qihan.zhao@outlook.com>
2022-05-06 21:24:51 +0800

    format output

--- ProcessManager.cpp
+++ ProcessManager.cpp
@@ -2,7 +2,7 @@
 #include <windows.h>
 
 #define MAX 5  // 定义最大进程数
-#define TIME 2 // 定义时间片大小，单位：秒（s） 
+#define TIME 6 // 定义时间片大小，单位：秒（s） 
 
 using namespace std;
 
@@ -20,6 +20,7 @@ typedef struct pcb {
     char state[6];
 }pcb, *pPCB;
 
+//进程组织：链接方式，here采用双向链表（带头节点）
 typedef struct PCBNode {
 	pPCB pcb;
 //	上一个PCB
@@ -30,14 +31,14 @@ typedef struct PCBNode {
 
 pPCBNode ReadyList = new PCBNode;
 
-
 // 打印输出PCB链表，直观展现各个PCB状态 
 void showPCB() {
 	pPCBNode p = ReadyList->next;
-    printf("id    到达时间    所需时间    已用时间    状态\n"); 
+    printf("id    |到达时间    |所需时间    |已用时间    |状态\n");
+    printf("---------------------------------------------------\n"); 
 	while(p) {
 		pPCB pcb = p->pcb;
-		printf("%-6d%-12d%-12d%-12d%-12s\n", 
+		printf("%-6d|%-12d|%-12d|%-12d|%-12s\n", 
 			pcb->id, pcb->arrivedTime, pcb->needTime, pcb->usedTime, pcb->state);
 		p = p->next;
 	}
@@ -58,7 +59,7 @@ void initPCB() {
         temp->next = NULL;
         p->next = temp;
         p = temp;
-//      头结点的before用来记录当前链表的尾结点（避免还要单独建一个tail标记尾结点） 
+        //头结点的before用来记录当前链表的尾结点（避免还要单独建一个tail标记尾结点） 
         ReadyList->before = temp;
     }
     printf("初始化后的ReadyList：\n");
@@ -67,7 +68,7 @@ void initPCB() {
 }
 
 // 检查ReadyList中是否还有未执行的PCB
-pPCBNode checkReadyList(pPCBNode &ReadyList) {
+pPCBNode checkReadyList() {
 	pPCBNode p = ReadyList->next;
 	while(p) {
 		if (strcmp(p->pcb->state, "Ready") == 0) {
@@ -93,20 +94,28 @@ pPCBNode checkPCB(pPCBNode &p) {
 // 运行 
 void Running() {
 	pPCBNode p = NULL;
-	while(p = checkReadyList(ReadyList)) {
-		checkPCB(p);
+	while(p = checkReadyList()) {
+		// checkPCB(p);
 		if (p != NULL) {
 			printf("此次要执行的PCB的id=%d\n", p->pcb->id);
 			strcpy(p->pcb->state, "Running");
 			printf("执行中......\n");
 			showPCB();
-			if ((p->pcb->needTime - p->pcb->usedTime) <= TIME) {
-				cout << mycount++;
-				Sleep(10000);
+            cout << mycount++;
+
+            int remainingTime = p->pcb->needTime - p->pcb->usedTime;
+			if (remainingTime <= TIME) {
+				Sleep(remainingTime * 1000);
 				strcpy(p->pcb->state, "Dead");
 				p->pcb->usedTime = p->pcb->needTime;
 				printf("执行完毕，id=%d的PCB状态转为“Dead”\n", p->pcb->id);
+                if (p->next) {
+					p->before->next = p->next;
+					p->next->before = p->before;
+					delete p;
+				}
 			} else {
+                Sleep(TIME * 1000);
 				strcpy(p->pcb->state, "Ready");
 				p->pcb->usedTime += TIME;
 //				当前未执行完毕的PCB连接到链表尾
@@ -121,7 +130,7 @@ void Running() {
 				printf("时间片到，还未执行完毕，id=%d的PCB状态转为“Ready”\n", p->pcb->id);
 			}
 		}
-		showPCB();
+		// showPCB();
 		printf("\n");
 	}
 }
