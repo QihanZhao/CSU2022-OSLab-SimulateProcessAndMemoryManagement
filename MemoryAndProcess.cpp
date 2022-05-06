abba21463b6d34f5638cc84baa579f32023ee0f3
HanX-gthb <qihan.zhao@outlook.com>
2022-05-06 21:43:22 +0800

    V1.0 beautify code

--- MemoryAndProcess.cpp
+++ MemoryAndProcess.cpp
@@ -1,4 +1,4 @@
-//完成版
+//完成版 & 代码美化
 #include<bits/stdc++.h> 
 #include <mutex> 
 #include <windows.h>
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+//----------------------------------------------------------------------PCB
 //进程标识：PCB
 typedef struct PCB {
     int id;
@@ -36,138 +37,36 @@ void creatPCB();                //进程创建原语
 void block(int id);             //进程阻塞原语
 void wakeup(int id);            //进程唤醒原语
 
-//OS中的工具函数
-void initPCB();                 // 初始化PCB
-void showPCB();                 // 打印输出PCB链表，直观展现各个PCB状态 
-pPCBNode checkReadyList();      // 检查ReadyList中是否还有未执行的PCB
-void startOS();                 // 启动OS 
-
-//全局变量
-mutex mtx;                          //内核数据结构（PCB链）的互斥锁
+//全局变量forPCB
 pPCBNode ReadyList = new PCBNode;   //内核数据结构（PCB链）
+mutex mtx;                          //内核数据结构（PCB链）的互斥锁
 int pcbID;                          //创建PCB时，用于产生pcb的id
-clock_t beginTime;                   //创建PCB时，用于产生达到时间
+clock_t beginTime;                  //创建PCB时，用于产生达到时间
 
-
+//----------------------------------------------------------------------Mem
 //空白内存标识：FMPT(FreeMemoryPartitionTable)
 typedef struct FreeMemNode {
     int size;
     int startAddress;
 }FreeMemNode, *pFreeMemNode;
 
-vector<FreeMemNode> FMPT;
-vector<FreeMemNode> test;
+//内存管理
+int allocate(int memoryNeeded);                             //内存分配
+void releaseAndMerge(int toReleasedSize, int startAddress);  //内存回收
 
+//全局变量forMem
+vector<FreeMemNode> FMPT;           //空闲分区表
 
+//----------------------------------------------------------------------OS
+//OS中的工具函数
+void initPCB();                 // 初始化PCB 
+void showPCB();                 // 打印输出PCB链表，直观展现各个PCB状态 
+void initMem();                 // 初始化内存 
+void showMem();                 // 打印输出空闲分区表 
+void initOS();                  // 初始化OS
+void startOS();                 // 启动OS 
+pPCBNode checkReadyList();      // 检查ReadyList中是否还有未执行的PCB 
 
-int allocate(int memoryNeeded){
-	int tmp = -1;
-    for(int i= 0 ;i< FMPT.size();i++ ){
-        if(FMPT[i].size >= memoryNeeded){
-            tmp = FMPT[i].startAddress;
-            FMPT[i].size -= memoryNeeded;
-            FMPT[i].startAddress += memoryNeeded;
-            break;
-        }
-    }
-//    cout<<"in the allocate func  "<<tmp<<"   "<< memoryNeeded <<endl;
-	if(tmp==-1) puts("Allocation Failure");
-    return tmp;
-}
-
-void releaseAndMerge(int toReleasedSize, int startAddress){
-	
-//	cout<<"in the Merge func  "<<startAddress<<"   "<< toReleasedSize <<endl;
-	
-    int endAddress = startAddress + toReleasedSize ; 
-    
-    if( FMPT[0].startAddress  >= startAddress ){
-        if(FMPT[0].startAddress  == endAddress){
-            FMPT[0].size += toReleasedSize;
-            FMPT[0].startAddress -= toReleasedSize;
-        }else{
-            FreeMemNode tmp = {.size = toReleasedSize, .startAddress = startAddress };
-            FMPT.insert(FMPT.begin(),tmp);
-        }
-    }else if(FMPT[FMPT.size()-1].startAddress < startAddress){
-        if(FMPT[FMPT.size()-1].startAddress + FMPT[FMPT.size()-1].size  == startAddress){
-            FMPT[FMPT.size()-1].size += toReleasedSize;
-        }else{
-            FreeMemNode tmp = {.size = toReleasedSize, .startAddress = startAddress };
-            FMPT.push_back(tmp);
-        }
-    }else{
-		for( int i= 0; i< FMPT.size()-1;i++ ){
-		    	if(FMPT[i].startAddress < startAddress && FMPT[i+1].startAddress > startAddress){
-		            if(FMPT[i].startAddress + FMPT[i].size  == startAddress ){
-		                //回收区之前 & 之后有相邻的空闲分区
-		                if(FMPT[i+1].startAddress == endAddress){
-		                    FMPT[i].size = FMPT[i].size + toReleasedSize + FMPT[i+1].size;
-		                    FMPT.erase(FMPT.begin() + i+1);
-		                }//仅回收区之前 有相邻的空闲分区
-		                else{
-		                    FMPT[i].size += toReleasedSize;
-//		                    puts("success");
-		                }
-		            }//仅回收区之后有相邻的空闲分区
-		            else if(FMPT[i].startAddress  == endAddress){
-		                FMPT[i].size += toReleasedSize;
-		                FMPT[i].startAddress -= toReleasedSize;
-		            }//回收区之前 & 之后都没有相邻的空闲分区
-		            else{
-		                FreeMemNode tmp = {.size = toReleasedSize, .startAddress = startAddress };
-		                FMPT.insert(FMPT.begin()+i+1,tmp);
-		            }
-		
-		        }
-		    }	
-	}
-
-    
-    
-}
-
-//总内存1000Mb，内核区
-void initMem(){
-    FreeMemNode tmp = {.size = MEMORYSIZE - KERNELSIZE, .startAddress = KERNELSIZE };
-    FMPT.push_back(tmp);
-}
-
-void showMem(){
-	printf("id    |内存始址    |空闲大小       <-空闲分区表\n");
-	printf("-----------------------------\n"); 
-	
-    for(int i= 0 ;i< FMPT.size();i++ ){
-        printf("%-6d|%-12d|%-12d\n", i,FMPT[i].startAddress, FMPT[i].startAddress);
-    }
-    puts("");
-}
-
-void initOS(){
-    //初始化内存
-    initMem();
-    //模拟一个随机分布的空白内存表
-    for( int i=0;i<15;i++){
-        int memSize = rand()%100 +1;
-        // cout<<memSize<<endl;
-        
-        int startAddress = allocate(memSize);
-        FreeMemNode tmp = {.size = memSize, .startAddress = startAddress };
-        test.push_back(tmp);
-    }
-    for( int i=1;i<14;i=i+2){
-        releaseAndMerge(test[i].size,test[i].startAddress);
-    }
-    puts("\n**********初始内存分配**********"); 
-    showMem();
-    
-    //初始化PCB
-    initPCB();
-    puts("\n**********初始提交的任务清单**********"); 
-    showPCB();
-    
-
-}
 
 int main() {
 
@@ -179,7 +78,7 @@ int main() {
 	
 	string s;
 	while(1){
-		printf("\n[myshell]# ");
+		printf("\n[HanShell]# ");
 		
 //	  	cin>>s;		
 		getline(cin,s);
@@ -206,21 +105,6 @@ int main() {
 	}
 	
 	return 0;
-}
-
-
-// 打印输出PCB链表，直观展现各个PCB状态 
-void showPCB() {
-	pPCBNode p = ReadyList->next;
-    printf("PID    |到达时间    |所需时间    |已用时间    |状态        |内存始址    |占用大小\n");
-    printf("----------------------------------------------------------------------------------\n"); 
-	while(p) {
-		PCB pcb = p->pcb;
-		printf("%-6d|%-12d|%-12d|%-12d|%-12s|%-12d|%-12d\n", 
-			pcb.id, pcb.arrivedTime, pcb.requiredTime, pcb.usedTime, pcb.state, pcb.startAddress, pcb.size );
-		p = p->next;
-	}
-	puts("");
 }
 
 // 初始化PCB
@@ -250,6 +134,39 @@ void initPCB() {
     
 }
 
+// 打印输出PCB链表，直观展现各个PCB状态 
+void showPCB() {
+	pPCBNode p = ReadyList->next;
+    printf("PID    |到达时间    |所需时间    |已用时间    |状态        |内存始址    |占用大小\n");
+    printf("----------------------------------------------------------------------------------\n"); 
+	while(p) {
+		PCB pcb = p->pcb;
+		printf("%-6d|%-12d|%-12d|%-12d|%-12s|%-12d|%-12d\n", 
+			pcb.id, pcb.arrivedTime, pcb.requiredTime, pcb.usedTime, pcb.state, pcb.startAddress, pcb.size );
+		p = p->next;
+	}
+	puts("");
+}
+
+
+
+//总内存1000Mb，内核区
+void initMem(){
+
+    FreeMemNode tmp = {.size = MEMORYSIZE - KERNELSIZE, .startAddress = KERNELSIZE };
+    FMPT.push_back(tmp);
+}
+
+void showMem(){
+	printf("id    |内存始址    |空闲大小       <-空闲分区表\n");
+	printf("-----------------------------\n"); 
+	
+    for(int i= 0 ;i< FMPT.size();i++ ){
+        printf("%-6d|%-12d|%-12d\n", i,FMPT[i].startAddress, FMPT[i].startAddress);
+    }
+    puts("");
+}
+
 // 检查ReadyList中是否还有未执行的PCB
 //Tip:多出口的函数，在互斥量的控制时要非常注意，在每个出口前都要unlock 
 pPCBNode checkReadyList() {
@@ -264,6 +181,30 @@ pPCBNode checkReadyList() {
 	}
     
 	return NULL;
+}
+
+void initOS(){
+    //初始化内存
+    initMem();
+    //模拟一个随机分布的空白内存表
+    vector<FreeMemNode> test;
+    for( int i=0 ; i<15 ; i++){
+        int memSize = rand()%100 +1;
+        // cout<<memSize<<endl;
+        int startAddress = allocate(memSize);
+        FreeMemNode tmp = {.size = memSize, .startAddress = startAddress };
+        test.push_back(tmp);
+    }
+    for( int i=1 ; i<14 ; i=i+2){
+        releaseAndMerge(test[i].size,test[i].startAddress);
+    }
+    puts("\n**********初始内存分配**********"); 
+    showMem();
+    
+    //初始化PCB
+    initPCB();
+    puts("\n**********初始提交的任务清单**********"); 
+    showPCB();
 }
 
 void startOS() {
@@ -392,4 +333,68 @@ void wakeup(int id){
 	    //不存在指定要唤醒的进程
 		puts("error");
 	mtx.unlock();   
+}
+
+int allocate(int memoryNeeded){
+	int tmp = -1;
+    for(int i= 0 ;i< FMPT.size();i++ ){
+        if(FMPT[i].size >= memoryNeeded){
+            tmp = FMPT[i].startAddress;
+            FMPT[i].size -= memoryNeeded;
+            FMPT[i].startAddress += memoryNeeded;
+            break;
+        }
+    }
+    // cout<<"in the allocate func  "<<tmp<<"   "<< memoryNeeded <<endl;
+	if(tmp==-1) puts("Allocation Failure");
+    return tmp;
+}
+
+void releaseAndMerge(int toReleasedSize, int startAddress){
+	
+//	cout<<"in the Merge func  "<<startAddress<<"   "<< toReleasedSize <<endl;
+	
+    int endAddress = startAddress + toReleasedSize ; 
+    
+    if( FMPT[0].startAddress  >= startAddress ){
+        if(FMPT[0].startAddress  == endAddress){
+            FMPT[0].size += toReleasedSize;
+            FMPT[0].startAddress -= toReleasedSize;
+        }else{
+            FreeMemNode tmp = {.size = toReleasedSize, .startAddress = startAddress };
+            FMPT.insert(FMPT.begin(),tmp);
+        }
+    }else if(FMPT[FMPT.size()-1].startAddress < startAddress){
+        if(FMPT[FMPT.size()-1].startAddress + FMPT[FMPT.size()-1].size  == startAddress){
+            FMPT[FMPT.size()-1].size += toReleasedSize;
+        }else{
+            FreeMemNode tmp = {.size = toReleasedSize, .startAddress = startAddress };
+            FMPT.push_back(tmp);
+        }
+    }else{
+		for( int i= 0; i< FMPT.size()-1;i++ ){
+		    	if(FMPT[i].startAddress < startAddress && FMPT[i+1].startAddress > startAddress){
+		            if(FMPT[i].startAddress + FMPT[i].size  == startAddress ){
+		                //回收区之前 & 之后有相邻的空闲分区
+		                if(FMPT[i+1].startAddress == endAddress){
+		                    FMPT[i].size = FMPT[i].size + toReleasedSize + FMPT[i+1].size;
+		                    FMPT.erase(FMPT.begin() + i+1);
+		                }//仅回收区之前 有相邻的空闲分区
+		                else{
+		                    FMPT[i].size += toReleasedSize;
+		                    // puts("success");
+		                }
+		            }//仅回收区之后有相邻的空闲分区
+		            else if(FMPT[i].startAddress  == endAddress){
+		                FMPT[i].size += toReleasedSize;
+		                FMPT[i].startAddress -= toReleasedSize;
+		            }//回收区之前 & 之后都没有相邻的空闲分区
+		            else{
+		                FreeMemNode tmp = {.size = toReleasedSize, .startAddress = startAddress };
+		                FMPT.insert(FMPT.begin()+i+1,tmp);
+		            }
+		
+		        }
+		    }	
+	}
 }
